108a4ebe300f57ccc63cc6b5efde84930d54f763
Ahmed Reza <mustafareza47@gmail.com>
2023-11-27 01:16:18 +0530

    creating react project, understanding the react flow and structure, JSX

---
+++ Day_1/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Chai    from './Chai'
+
+function App() {
+  return (
+    <>
+      <h1>Chai aur React Vite | Ahmed Reza</h1>
+      <Chai />
+    </>
+  )
+}
+
+export default App
---
+++ Day_1/vite-project/src/Chai.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chai = () => {
+  return (
+    <div>
+      <p>I am ready to learn React.</p>
+    </div>
+  )
+}
+
+export default Chai
