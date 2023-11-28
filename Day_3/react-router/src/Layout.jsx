c21169f69ff29443f00c97040b62e533ad3ffbb9
Ahmed Reza <mustafareza47@gmail.com>
2023-11-28 22:14:49 +0530

    understanding react router dom, custom hooks

---
+++ Day_3/react-router/src/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Home from './components/home/Home'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+
+    </>
+  )
+}
+
+export default Layout
